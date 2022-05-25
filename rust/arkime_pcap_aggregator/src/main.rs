6ab0f3d92c8573eb03bad23945d890bb9c32f2ab
Matsunaga Yutaro <matsunagayutaro@team-lab.com>
2022-05-25 10:07:24 +0900

    エラー修正完了

--- rust/arkime_pcap_aggregator/src/main.rs
+++ rust/arkime_pcap_aggregator/src/main.rs
@@ -76,53 +76,62 @@ fn main() {
     // ".../localhost-220508-00001297.pcap"のようなファイル名を想定
     let re = Regex::new(r"/.+-(\d{6})-(\d{8})\.pcap$").unwrap();
 
+    #[derive(Debug)]
     struct FileEntry {
         filename: String,
         fileid: u32,
-        pcap_date: chrono::DateTime<FixedOffset>,
+        pcap_date: chrono::NaiveDate,
     }
     let mut pcap_list: Vec<FileEntry> = dirs
         .filter_map(|r| r.ok())
-        .map(|d| {            
+        .map(|d| {
             let path_str = d.path().into_os_string().into_string().unwrap();
             let cap_option = re.captures(&path_str);
 
-            if cap_option.is_none() || cap_option.unwrap().len() != 2 {
+            println!("path_str: {}", path_str);
+            println!("opt: {:?}", cap_option);
+
+            if cap_option.is_none() || cap_option.as_ref().unwrap().len() != 3 {
                 return None::<FileEntry>;
             }
             let cap = cap_option.unwrap();
-            
-            let pcap_date = DateTime::parse_from_str(&cap[0], "%y%m%d").unwrap();
-            Some(FileEntry{
-                filename: path_str,
-                fileid: cap[1].parse().unwrap(),
-                pcap_date: pcap_date,
+
+            NaiveDateTime::parse_from_str(&cap[1], "%y%m%d").unwrap();
+            let pcap_native_date = NaiveDate::parse_from_str(&cap[1], "%y%m%d").unwrap();
+            //let pcap_date = FixedOffset::east(9 * 3600).from_local_datetime(&pcap_native_date).unwrap();
+            Some(FileEntry {
+                filename: path_str.clone(),
+                fileid: cap[2].parse().unwrap(),
+                pcap_date: pcap_native_date,
             })
         })
         .filter(|o| o.is_some())
         .map(|o| o.unwrap())
         .collect();
-    
+
     pcap_list.sort_by(|a, b| a.fileid.partial_cmp(&b.fileid).unwrap());
+    println!("pcap_list dump: {:?}", pcap_list);
 
     // 前日の日付のファイルに一部集計対象のパケットが含まれている可能性があり、読み出し始めるファイルのindex
     let mut read_first_index = 0;
     // pcapの読み出しを終えるファイルのindex
     let mut read_last_index = pcap_list.len() - 1;
     {
+        let first_naive_date = first_datetime.naive_local().date();
+        let last_naive_date = last_datetime.naive_local().date();
         let mut searching_first_index = true;
         for (i, e) in pcap_list.iter().enumerate() {
             if searching_first_index {
-                if e.pcap_date > first_datetime {
+                if e.pcap_date > first_naive_date {
                     continue;
-                }else {
+                } else {
                     read_first_index = i - 1;
                     searching_first_index = false;
                 }
-            }else {
-                if e.pcap_date < last_datetime {
+            } else {
+                if e.pcap_date < last_naive_date {
                     continue;
-                }else {
+                } else {
                     read_last_index = i - 1;
                     break;
                 }
@@ -136,9 +145,10 @@ fn main() {
 
     let mut prev_arrival = 0;
 
-
     // pcapごとの大きなループ
-    for path in pcap_list[read_first_index..read_last_index].iter().map(|e| e.filename)
+    for path in pcap_list[read_first_index..read_last_index]
+        .iter()
+        .map(|e| &e.filename)
     {
         println!("read from {:?}", path);
 
@@ -146,8 +156,7 @@ fn main() {
             Err(e) => {
                 println!(
                     "failed to open pcap file; skip this file: {}, reason: {}",
-                    path,
-                    e
+                    path, e
                 );
                 continue;
             }
